a571cbde8efcb9bd42e84e7a46b83f40ac2a7ba6
Ruby M <ranmi.chaos.fox@gmail.com>
2019-03-12 20:23:28 -0400

    broken; about to convert other arrays to linked lists

--- Triangulating.java
+++ Triangulating.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.ArrayList;
+
 class Triangulating{
 
     public static void main(String[] args){
@@ -10,9 +14,12 @@ class Triangulating{
     }
 
     public void run(){
-        int[][] vertices4 = new int[4][4];
-        vertices4[0][0] = 2;
-        vertices4[2][0] = 0;
+
+        LinkedList<Integer>[] vertices4 = (LinkedList<Integer>[])new Object[4];
+        vertices4[0] = new LinkedList<Integer>();
+        vertices4[0].add(2);
+        vertices4[2] = new LinkedList<Integer>();
+        vertices4[2].add(0);
         int[] connection_numbers_4 = new int[4];
         connection_numbers_4[0] = 1;
         connection_numbers_4[1] = 0;
@@ -20,9 +27,11 @@ class Triangulating{
         connection_numbers_4[3] = 0;
         Triangulation t = new Triangulation(vertices4, connection_numbers_4, (int)0, (int)4);
 
-        int[][] vertices4_2 = new int[4][4];
-        vertices4_2[1][0] = 3;
-        vertices4_2[3][0] = 1;
+        LinkedList<Integer>[] vertices4_2 = (LinkedList<Integer>[])new Object[4];
+        vertices4_2[1] = new LinkedList<Integer>();
+        vertices4_2[1].add(3);
+        vertices4_2[3] = new LinkedList<Integer>();
+        vertices4_2[3].add(1);
         int[] connection_numbers_4_2 = new int[4];
         connection_numbers_4_2[0] = 0;
         connection_numbers_4_2[1] = 1;
@@ -39,13 +48,12 @@ class Triangulating{
 
     private class Triangulation{
 
-        // change list of connections at each vertex to a linked list so we can easily insert mid-list? 
-        int[][] vertices;
+        LinkedList<Integer>[] vertices;
         int[] connection_numbers;
         int zeroindex;
         int size;
 
-        public Triangulation(int[][] _vertices, int[] _connection_numbers, int _zeroindex, int _size){
+        public Triangulation(LinkedList<Integer>[] _vertices, int[] _connection_numbers, int _zeroindex, int _size){
             vertices = _vertices;
             zeroindex = _zeroindex;
             size = _size;
@@ -53,18 +61,22 @@ class Triangulating{
         }
 
         public Triangulation rotate(int n){
-            
+        
             int[] replacement_key = new int[size];
             for(int i=0; i<size; i++){
                 replacement_key[i] = (int)((i + n) % size);
             }
 
-            int[][] new_vertices = new int[size][size];
+            LinkedList<Integer>[] new_vertices = (LinkedList<Integer>[])new Object[size];
             for(int i=0; i<size; i++){
-                int[] new_connections = new int[size];
-                for(int j=0; j<connection_numbers[i]; j++){
-                    new_vertices[i][j] = replacement_key[vertices[i][j]];
+                if (connection_numbers[i] > 0){
+                    new_vertices[i] = new LinkedList<Integer>();
+                    ListIterator<Integer> iter = vertices[i].listIterator(0);
+                    for (int j=0; j<connection_numbers[i]; j++){
+                        new_vertices[i].add(replacement_key[iter.next()]);
+                    }
                 }
+
             }
 
             int new_zeroindex = int_modulo(zeroindex - n, size);
@@ -72,17 +84,39 @@ class Triangulating{
         }
 
         public void connect_vertices(int n1, int n2){
-            n1 = int_modulo((n1 + zeroindex), size);
-            n2 = int_modulo((n2 + zeroindex), size);
-            int i = 0;
-            while (i < vertices[n1].length && vertices[n1][i] < n2){
-                i += 1; 
+            int n1_index = int_modulo((n1 + zeroindex), size);
+            int n2_index = int_modulo((n2 + zeroindex), size);
+            if (vertices[n1_index] == null){
+                vertices[n1_index] = new LinkedList<Integer>();
             }
-            
+            if (vertices[n2_index] == null){
+                vertices[n2_index] = new LinkedList<Integer>();
+            }
+            ListIterator<Integer> iter1 = vertices[n1_index].listIterator();
+            while (iter1.hasNext()){
+                int n = iter1.next();
+                if (n <= n2){
+                    iter1.previous();
+                    break;
+                } 
+            }
+            iter1.add(n2);
+
+            ListIterator<Integer> iter2 = vertices[n2_index].listIterator();
+            while (iter2.hasNext()){
+                int n = iter2.next();
+                if (n <= n1){
+                    iter2.previous();
+                    break;
+                } 
+            }
+            iter2.add(n1);
         }
 
         public void add_ear(){
-
+            connect_vertices(zeroindex, int_modulo(zeroindex - 1, size));
+            // Make a copy starting from zero_index (so new zero_index is 0, and one bigger) and return?
+            // Or like ... make it all linked lists so insertion is easier .. what am I doing that's array specific? 
         }
 
         @Override
@@ -109,11 +143,17 @@ class Triangulating{
                 if (this.connection_numbers[this_index] != other.connection_numbers[other_index]){
                     return false;
                 }
-                for (int j = 0; j < this.connection_numbers[this_index]; j++){
-                    if (this.vertices[this_index][j] != other.vertices[other_index][j]){
-                        return false;
+
+                if (this.connection_numbers[this_index] > 0){
+                    ListIterator<Integer> iter1 = this.vertices[this_index].listIterator();
+                    ListIterator<Integer> iter2 = other.vertices[other_index].listIterator();
+                    for (int j=0; j<this.connection_numbers[this_index]; j++){
+                        if (iter1.next() != iter2.next()){
+                            return false;
+                        }
                     }
                 }
+
                 this_index +=1; 
                 other_index +=1;
             }
@@ -125,8 +165,11 @@ class Triangulating{
             String rtn = "zero index: " + zeroindex;
             for (int i=0; i<size; i++){
                 rtn = rtn + "\n" + i + ":";
-                for (int j=0; j<connection_numbers[i]; j++){
-                    rtn = rtn + vertices[i][j] + ",";
+                if (connection_numbers[i] > 0){
+                    ListIterator<Integer> iter = vertices[i].listIterator();
+                    while (iter.hasNext()){
+                        rtn = rtn + iter.next() + ",";
+                    }
                 }
             }
             return rtn; 
